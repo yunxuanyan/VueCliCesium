d6f04041a3f6bcfb562a1a325220c8b324dddd17
Jenny Yan <yunxuan.yan@gmail.com>
2019-03-29 10:29:39 +1300

    add cesium into the project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CesiumMap></CesiumMap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CesiumMap from './components/CesiumMap.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CesiumMap
   }
 }
 </script>
---
+++ src/components/CesiumMap.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="cesiumContainer"></div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'CesiumMap',
+    mounted() {
+        let Cesium = window.cesium;
+        Cesium.Ion.defaultAccessToken = '[YOUR_CESIUM_TOKEN]';
+        var viewer = new Cesium.Viewer('cesiumContainer');
+    }
+}
+</script>
+
+<style scoped>
+.viewer {
+  width: 100%;
+  height: 400px;
+}
+<style>
